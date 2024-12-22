53c4e1979d2bb671408e35513da88011129f0a44
MarcosMonteiro <116771883+MonteiroMarcoss@users.noreply.github.com>
2024-12-22 01:39:56 -0400

    Update data_cleaning.R

--- data_cleaning.R
+++ data_cleaning.R
@@ -5,10 +5,10 @@ library(tsibble)
 
 # variaveis macro ---------------------------------------------------------
 
-ids <- c('pib' = 24363, #índice
+ids <- c('pib' = 24363, # em nível
          'inflacao' = 433, #v% mensal
          'juros' = 4189, #v% anual
-         'cambio' = 3698) # umc/dólar
+         'cambio' = 3698) # umc/dólar, em nível
 
 cov_macro <- GetBCBData::gbcbd_get_series(
  id = ids,
@@ -37,11 +37,6 @@ bancos <- cadastro %>%
 base <- base %>%
   filter(CNPJ %in% bancos) %>%
   drop_na()
-
-#dic <- base %>%
- # distinct(CONTA, `NOME CONTA`)
-
-#dic %>% filter(grepl("11000006", `NOME CONTA`))
 
 painel_full <- base %>%
   select(-`NOME CONTA`, -DOCUMENTO) %>%
@@ -74,11 +69,6 @@ df_TG <- readxl::read_xlsx('df_TG.xlsx')|>
  mutate(Data = as.Date(Data)) |> select(-selic) |>
  mutate(dummy_tg = ifelse(taylor_gap > 0, 1, 0))
 
-
-# diferença meta - inflação -----------------------------------------------
-
-#meta_inflacao <- readxl::read_xlsx('meta.xlsx')
-
 # Painel completo -------------------------------------------------------------------
 
 painel_full <- painel_full |> 
@@ -87,10 +77,6 @@ painel_full <- painel_full |>
  inner_join(df_TG, by = 'Data') 
 
 painel_full <- pdata.frame(painel_full, index = c('CNPJ', 'Data'))
-
-# número de bancos
-#n_b <- painel_full |> pull(Banco) |> unique() 
-#glimpse(n_b)
 
 # Painel bancos S1 (banco grande) --------------------------------------------
 
@@ -236,74 +222,3 @@ painel_s4 <- painel_s4 |>
  inner_join(df_TG)
 
 painel_s4 <- pdata.frame(painel_s4, index = c('CNPJ', 'Data'))
-
-# tabela descritiva -------------------------------------------------------
-
-tab <- painel_full |>
- select(roa, roe, ativo, ativos_liquidos, receitas_sobre_ativo,
-        lucro.prejuizo_acumulado, depositos)
-
-tab_desc <- skim(tab)
-tab_desc <- tab_desc |> select(skim_variable,
-                               numeric.mean,
-                               numeric.sd,
-                               numeric.p0, 
-                               numeric.p25,
-                               numeric.p50,
-                               numeric.p75,
-                               numeric.p100) |>
- mutate(max = max(painel_full$roa, na.rm = T))
-
-
-a <- tab_desc |> pivot_longer(
- names_to = 'estatisticas',
- values_to = 'valores',
- cols = everything())
-
-xtable::xtable(tab_desc)
-# desc_omitir -------------------------------------------------------------
-
-
-tab_desc <- painel_full |>
- summarise(roa_media = mean(roa, na.rm = T),
-           roa_desviop = sd(roa, na.rm = T),
-           roa_min = min(roa, na.rm = T),
-           roa_max = max(roa, na.rm = T),
-           roe_media = mean(roe, na.rm = T),
-           roe_desviop = sd(roe, na.rm = T),
-           roe_min = min(roe, na.rm = T),
-           roe_max = max(roe, na.rm = T),
-           ativo_media = mean(ativo, na.rm = T),
-           ativo_desviop = sd(ativo, na.rm = T),
-           ativo_min = min(ativo, na.rm = T),
-           ativo_max = max(ativo, na.rm = T),
-           pl_media = mean(pl_sobre_ativo, na.rm = T),
-           pl_desviop = sd(pl_sobre_ativo, na.rm = T),
-           pl_min = min(pl_sobre_ativo, na.rm = T),
-           pl_max = max(pl_sobre_ativo, na.rm = T),
-           rt_media = mean(receitas_sobre_ativo, na.rm = T),
-           rt_desviop = sd(receitas_sobre_ativo, na.rm = T),
-           rt_min = min(receitas_sobre_ativo, na.rm = T),
-           rt_max = max(receitas_sobre_ativo, na.rm = T),
-           lucro_media = mean(lucro.prejuizo_acumulado, na.rm = T),
-           lucro_desviop = sd(lucro.prejuizo_acumulado, na.rm = T),
-           lucro_min = min(lucro.prejuizo_acumulado, na.rm = T),
-           lucro_max = max(lucro.prejuizo_acumulado, na.rm = T),
-           ativos_liquidos_media = mean(ativos_liquidos, na.rm = T),
-           ativos_liquidos_desviop = sd(ativos_liquidos, na.rm = T),
-           ativos_liquidos_min = min(ativos_liquidos, na.rm = T),
-           ativos_liquidos_max = max(ativos_liquidos, na.rm = T),
-           depositos_media = mean(depositos, na.rm = T),
-           depositos_desviop = sd(depositos, na.rm = T),
-           depositos_min = min(depositos, na.rm = T),
-           depositos_max = max(depositos, na.rm = T)
-           ) |>
- pivot_longer(
-  cols = everything(),
-  names_to = c("variable", ".value"),
-  names_pattern = "(.*)_(.*)"
- )
-
-#writexl::write_xlsx(tab_desc, 'tab_desc.xlsx')
-
-
